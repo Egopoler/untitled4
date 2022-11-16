2fcdb71447f173a6fd610207ba7d58115e4141b5
Egopoler <egpol2004@yandex.ru>
2022-11-16 17:20:49 +0300

    eee

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.io.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("fff");
+        try(FileInputStream in = new FileInputStream("input.txt");
+            FileOutputStream out = new FileOutputStream("output.txt"))
+        {
+            byte[] buffer = new byte[in.available()];
+            in.read(buffer, 0, buffer.length);
+            out.write(buffer, 0, buffer.length);
+        } catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
